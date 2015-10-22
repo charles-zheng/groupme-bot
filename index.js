2c20a05cb5258901a63ffdea9a35705368d00470
jmatty1983 <jmatty1983@gmail.com>
2015-10-21 23:32:26 -0500

    Fix cool ascii bug

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var http, director, cool, bot, router, server, port;
 
 http        = require('http');
 director    = require('director');
-cool        = require('cool-ascii-faces');
 bot         = require('./bot.js');
 
 router = new director.http.Router({
