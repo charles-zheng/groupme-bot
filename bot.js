fd4d57a1de55c6f7fef3ec5425088f36d3609fb4
jmatty1983 <jmatty1983@gmail.com>
2015-10-21 23:35:09 -0500

    Fix logging

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/welcome$/;
 
-  console.log("request");
+  console.log(request);
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
