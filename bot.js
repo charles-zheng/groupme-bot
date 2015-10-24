bf6aceed604aaf1370930f7916bd7372d65652b5
jmatty1983 <jmatty1983@gmail.com>
2015-10-23 21:33:28 -0500

    Hard code leaders to be paged on @leader request

--- bot.js
+++ bot.js
@@ -6,19 +6,43 @@ function respond() {
   var request   = JSON.parse(this.req.chunks[0]),
       newMember = / added /,
       leader    = /^@leaders$/;
+      troll     = /^@troll$/;
 
   this.res.writeHead(200);
   this.res.end();
 
-  if(debug && typeof(request.attachments[0]) !== 'undefined')
+
+  if(debug && typeof(request.attachments[0]) !== 'undefined') {
+    console.log(request);
+    console.log(request.attachments[0].loci);
     console.log(request.attachments[0].user_ids);
+  }
 
   if(request.system) {
     if(request.text && newMember.test(request.text)) {
-      postMessage("Welcome to Reddit Asylum! This is our social chat. Please change your name to match your IGN. If you haven't already, make sure to read our rules available at https://www.reddit.com/r/RedditAsylumCoC/wiki/index.", []);
+      setTimeout(postMessage("Welcome to Reddit Asylum! This is our social chat. Please change your name to match your IGN. If you haven't already, make sure to read our rules available at https://www.reddit.com/r/RedditAsylumCoC/wiki/index.", []), 3000);
     }
   } else if (request.text && leader.test(request.text)) {
-    postMessage("@Fo0 just testing", [ {type: 'mentions', user_ids: ['30802922'] } ]);
+    setTimeout(postMessage("Hey leaders you're being paged! [@GusGus @Michael @Lee1104 @Ryan @JLU @Fowla @Champ @Wiiii @Blareposeidon2082 @Bucket @419GottaMinute]",
+                [
+                  {
+                    loci: [ [33, 7], [41, 8], [50, 8], [59, 5], [65, 4], [70, 6],
+                            [77, 6], [84, 6], [91, 18], [110, 7], [118, 15] ],
+                    type: 'mentions',
+                    user_ids: ['15032435', '26027141', '22092647', '11713960',
+                               '24727461', '25070550', '19112175', '12076411',
+                               '23434057', '27333432', '19571350']
+                  }
+                ]), 1000);
+  } else if (request.text && troll.test(request.text)) {
+    setTimeout(postMessage("Troll you're being summoned [@SonOfGusGus (Asylum)]",
+                [
+                  {
+                    loci: [ [29, 20] ],
+                    type: 'mentions',
+                    user_ids: [ '25478014' ]
+                  }
+                ]), 1000);
   }
 }
 
@@ -32,9 +56,9 @@ function postMessage(botResponse, attachments) {
   };
 
   body = {
-    "attachments": attachments,
-    "bot_id" : botID,
-    "text"   : botResponse
+    "attachments" : attachments,
+    "bot_id"      : botID,
+    "text"        : botResponse
   };
 
   console.log('sending ' + botResponse + ' to ' + botID);
