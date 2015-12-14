fbbc09462dc0f1f4da2392b6be8608eb30eae804
jmatty1983 <jmatty1983@gmail.com>
2015-12-14 13:22:22 -0600

    Add dark bot settings

--- config/config.js
+++ config/config.js
@@ -5,16 +5,23 @@ exports.bot_owner = {
   id:   process.env.BOT_OWNER_ID //'30802922'
 };
 
-exports.bots = {
-  ra: process.env.RA_BOT_ID,
-  raw: process.env.RAW_BOT_ID,
-  ral: process.env.RAL_BOT_ID,
-  fo0: process.env.FO0_BOT_ID,
-  ralv: process.env.RALV_BOT_ID,
-  rar: process.env.RAR_BOT_ID,
-  rabb: process.env.RABB_BOT_ID,
-  bmc: process.env.BMC_BOT_ID,
-  test: process.env.some_var_not_avail
-};
+var botName = process.env.BOT_NAME;
+
+if (botName == 'asylum') {
+  exports.bots = {
+    ra: process.env.RA_BOT_ID,
+    raw: process.env.RAW_BOT_ID,
+    ral: process.env.RAL_BOT_ID,
+    fo0: process.env.FO0_BOT_ID,
+    ralv: process.env.RALV_BOT_ID,
+    rar: process.env.RAR_BOT_ID,
+    rabb: process.env.RABB_BOT_ID,
+    bmc: process.env.BMC_BOT_ID
+  };
+} else if (botName == 'dark') {
+  exports.bots = {
+    bffs: process.env.BFFS_BOT_ID
+  };
+}
 
 exports.delay_time = 1000;
