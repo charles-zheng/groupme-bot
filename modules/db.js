e3f18c0d04cb9ef6aee8381192993f8d7120e676
charles-zheng <charles312@gmail.com>
2016-12-17 21:41:09 -0500

    committed

--- modules/db.js
+++ modules/db.js
@@ -2,13 +2,16 @@ var mongoDB     = require('mongodb').MongoClient;
 
 var connection_string = 'mongodb://127.0.0.1:27017/nodejs';
 
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+/*if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
   connection_string = 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
   process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
   process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
   process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
   process.env.OPENSHIFT_APP_NAME;
-}
+}*/
+
+connection_string = 'mongodb://heroku_2zvnddw1:q5ugtthkftjlrhrmqlsvhsb4nl@ds147377.mlab.com:47377/heroku_2zvnddw1'
+
 
 function connect(callback){
   mongoDB.connect(connection_string, function(err, db) {
