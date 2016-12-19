7d7bdf663dbd8a33a0455b4ef525dd8b768559a4
charles-zheng <charles312@gmail.com>
2016-12-18 21:14:29 -0500

    added ron

---
+++ custom_commands/ron-swanson.js
@@ -0,0 +1,63 @@
+var fun_command = true;
+var cmds = [cmdRonSwanson];
+
+var HTTPS = require('https');
+
+exports.modName = "Ron Swanson";
+
+exports.checkCommands = function(dataHash, callback) {
+  cmds.some(function(cmd){
+    return cmd(dataHash.funMode, dataHash.request.text, callback);
+  });
+}
+
+exports.getCmdListDescription = function () {
+  var cmdArr = [
+    {cmd: "/ron", desc: "Responds with a random Ron Swanson quote.", fun: true}
+  ];
+
+  return cmdArr;
+}
+
+function cmdCatFact(funMode, request, callback){
+  var regex = /^\/ron/i;
+  
+  if (regex.test(request)){
+    if(!funMode){
+      callback(true, "Sorry I'm no fun right now.", []);
+      return "Sorry I'm no fun right now.";
+    }
+    
+    var val = regex.exec(request);
+
+    var options = {
+      hostname: "ron-swanson-quotes.herokuapp.com",
+      path: "/v2/quotes"
+    };
+  
+    var callbackAPI = function(response) {
+      var str = '';
+
+      response.on('data', function(chunk) {
+        str += chunk;
+      });
+
+      response.on('end', function() {
+        str = JSON.parse(str);
+        
+        var msg = str;
+        /*if (typeof(str.facts[0]) !== 'undefined'){
+          msg = str.facts[0];
+        } else {
+          msg = "I don't like cats.";
+        }*/
+
+        callback(true, msg, []);
+      });
+    };
+
+    HTTPS.request(options, callbackAPI).end();
+  } else {
+    return false;
+  }
+}
